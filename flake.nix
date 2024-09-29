9a5f4557c8d264efa826af4ad8fafc210766260d
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-09-29 21:50:04 +0200

    commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Example JavaScript development environment for Zero to Nix";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  # Flake outputs
+  outputs =
+    { nixpkgs, self }:
+
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+
+            # The Nix packages provided in the environment
+            packages = with pkgs; [
+              bun
+              nodejs_22
+              biome
+            ];
+          };
+        }
+      );
+    };
+}
